95efa6d3a4b23d8e34b478efd9fe228acdf04162
Janos Divenyi <janos.divenyi@emarsys.com>
2017-01-25 12:22:27 +0100

    add sublime files

---
+++ sublime/User/dash-till-end.py
@@ -0,0 +1,19 @@
+import sublime, sublime_plugin
+
+class CommenterSmallCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        sel = self.view.sel()[0]
+        line = self.view.line(sel)
+        [row, currentColumn] = self.view.rowcol(line.end())
+        while (currentColumn < 80):
+            self.view.insert(edit, self.view.text_point(row, currentColumn), "-")
+            currentColumn = currentColumn + 1
+
+class CommenterBigCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        sel = self.view.sel()[0]
+        line = self.view.line(sel)
+        [row, currentColumn] = self.view.rowcol(line.end())
+        while (currentColumn < 80):
+            self.view.insert(edit, self.view.text_point(row, currentColumn), "=")
+            currentColumn = currentColumn + 1
